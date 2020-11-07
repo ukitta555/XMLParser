b5675323fc2a22c9c9fb775565a80bcf0561bc30
Vladyslav Nekriach <ukitta555@gmail.com>
2020-11-07 19:22:27 -0200

    implemented class for progr. language

---
+++ XMLConverter/ProgLanguage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLConverter
+{
+    class ProgLanguage
+    {
+
+        public string LanguageName { get; set; } = "";
+        public string ReleaseYear { get; set; } = "";
+        public string Authors { get; set; } = "";
+        public string TypeOfLanguage { get; set; } = "";
+        public string AbstractionLevel { get; set; } = "";
+        public string CommonlyUsedFor { get; set; } = "";
+    }
+}
